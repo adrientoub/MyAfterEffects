9b59a7986f5879c4a3900d35d11fadff1f10a417
Adrien Toubiana <adrientoub@hotmail.com>
2016-07-10 19:10:07 +0200

    Add JFileChooser to choose video

--- src/controller/MenuBarController.java
+++ src/controller/MenuBarController.java
@@ -2,8 +2,10 @@ package controller;
 
 import view.MainWindow;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * Created by Adrien on 10/07/2016.
@@ -16,7 +18,12 @@ public class MenuBarController implements ActionListener {
                 System.exit(0);
                 break;
             case "New":
-                MainWindow.getMainWindow().getTabbedView().newTab();
+                JFileChooser jFileChooser = new JFileChooser();
+                jFileChooser.showOpenDialog(null);
+                File selected = jFileChooser.getSelectedFile();
+                if (selected == null)
+                    return;
+                MainWindow.getMainWindow().getTabbedView().newTab(selected);
                 break;
         }
     }
--- src/view/TabbedView.java
+++ src/view/TabbedView.java
@@ -3,15 +3,15 @@ package view;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 
 import javax.swing.*;
+import java.io.File;
 
 /**
  * Created by Adrien on 10/07/2016.
  */
 public class TabbedView extends JTabbedPane {
-    String videoFile = "test.mkv";
-    public void newTab() {
+    public void newTab(File selected) {
         EmbeddedMediaPlayerComponent mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
         addTab("Video", mediaPlayerComponent);
-        mediaPlayerComponent.getMediaPlayer().playMedia(videoFile);
+        mediaPlayerComponent.getMediaPlayer().playMedia(selected.getAbsolutePath());
     }
 }
