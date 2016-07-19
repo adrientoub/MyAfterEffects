01d69c7b0b2d22f0de6308a1edefa3d232e91949
Adrien Toubiana <adrientoub@hotmail.com>
2016-07-20 01:07:05 +0200

    Add comments on Media interface

--- src/manager/Media.java
+++ src/manager/Media.java
@@ -8,8 +8,28 @@ import java.awt.image.BufferedImage;
  * Created by Adrien on 19/07/2016.
  */
 public interface Media {
+    /**
+     * Returns the image at the selected frame.
+     * @param frameNb The frame number
+     * @return A filtered image
+     */
     BufferedImage getImage(int frameNb);
+
+    /**
+     * Add a filter to the Media
+     * @param filter the filter to add.
+     */
     void addFilter(Filter filter);
+
+    /**
+     * Get the duration of the Media on the Timeline
+     * @return the duration in milliseconds
+     */
     long getDuration();
+
+    /**
+     * Get the name of the Media
+     * @return the name of the Media
+     */
     String getName();
 }
